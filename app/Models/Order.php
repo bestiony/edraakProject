048979ca3eb0771c78cae8d1ba628ff198f50003
abderrahman fodili <bestiony@users.noreply.github.com>
2022-06-27 02:54:07 +0100

    create relationship between - product and image  1-1 - product and return policy m-1 - product and category m-1 - product and order   m-m - user and order 1-m

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
     use HasFactory;
+
+
+    public function user(){
+        return $this->belongsTo(User::class,'costumer_id');
+    }
+
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Image;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function image(){
+        return $this->hasOne(Image::class,'image_id');
+    }
+
+    public function returnPolicy(){
+        return $this->hasOne(ReturnPolicy::class,'return_policy_id');
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class,'category_id');
+    }
 }
