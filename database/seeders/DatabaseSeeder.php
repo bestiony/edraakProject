c4e273320038e9e53991328e0d9c03cc52bb321b
abderrahman fodili <bestiony@users.noreply.github.com>
2022-07-11 19:23:46 +0100

    remove testing data to prepare for real products data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -90,20 +90,20 @@ class DatabaseSeeder extends Seeder
 
             // ------------- users ------------
 
-            if (User::count()==0 ){
-                User::create([
-                    'first_name'=>'abderrahman',
-                    'last_name'=>'fodili',
-                    'email'=>'bestiony12373@gmail.com',
-                    'password'=> Hash::make('12345678')
-                ]);
-            }
+            // if (User::count()==0 ){
+            //     User::create([
+            //         'first_name'=>'abderrahman',
+            //         'last_name'=>'fodili',
+            //         'email'=>'bestiony12373@gmail.com',
+            //         'password'=> Hash::make('12345678')
+            //     ]);
+            // }
             if(Admin::count()==0){
-                Admin::create([
-                    'name'=>'bachir',
-                    'email'=>'bachirove@gmail.com',
-                    'password'=> Hash::make('87654321')
-                ]);
+                // Admin::create([
+                //     'name'=>'bachir',
+                //     'email'=>'bachirove@gmail.com',
+                //     'password'=> Hash::make('87654321')
+                // ]);
                 Admin::create([
                     'name'=>'Bread',
                     'email'=>'admin@edraakmc.com',
@@ -194,101 +194,101 @@ class DatabaseSeeder extends Seeder
     }
 
 
-    if(Image::count()== 0){
-        Image::create([
-            'id'=>4,
-            'image_url'=>'https://m.media-amazon.com/images/I/51aiBTahmbL._AC_UX466_.jpg'
-        ]);
-        Image::create([
-            'id'=>5,
-            'image_url'=>'https://m.media-amazon.com/images/I/61XE+thKb0L._AC_UY550_.jpg'
-        ]);
-        Image::create([
-            'id'=>6,
-            'image_url'=>'https://m.media-amazon.com/images/I/61H3RcZUPaL._AC_UY550_.jpg'
-        ]);}
+    // if(Image::count()== 0){
+    //     Image::create([
+    //         'id'=>4,
+    //         'image_url'=>'https://m.media-amazon.com/images/I/51aiBTahmbL._AC_UX466_.jpg'
+    //     ]);
+    //     Image::create([
+    //         'id'=>5,
+    //         'image_url'=>'https://m.media-amazon.com/images/I/61XE+thKb0L._AC_UY550_.jpg'
+    //     ]);
+    //     Image::create([
+    //         'id'=>6,
+    //         'image_url'=>'https://m.media-amazon.com/images/I/61H3RcZUPaL._AC_UY550_.jpg'
+    //     ]);}
         // -------------- seeding products ---------------
-        if(Product::count()==0){
-        // $faker = Faker::create();
-        $productsNames = ['something', 'somrthingelse', 'WOW','Amasing'];
-        $descriptions = [
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto accusamus, quam deleniti aut cumque, reprehenderit odio quibusdam earum numquam itaque quisquam labore cupiditate ullam hic doloribus autem nisi non?',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto accusamus, quam deleniti aut cumque, reprehenderit odio quibusdam earum numquam itaque quisquam labore cupiditate ullam hic doloribus autem nisi non?',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto accusamus, quam deleniti aut cumque, reprehenderit odio quibusdam earum numquam itaque quisquam labore cupiditate ullam hic doloribus autem nisi non?'
-    ];
-        for($i =1; $i <= 18; $i++){
-            $category = rand(1,3);
-            $product = [
-                'id'=>$i,
-                'name'=>$productsNames[rand(0,2)],
-                'description' => $descriptions[rand(0,2)],
-                'price'=> rand(20,1000),
-                'size' => rand(1,5),
-                'image_id'=> $category,
-                'return_policy_id'=> rand(1,4),
-                'category_id' => $category
-            ];
-            Product::create($product);
-        }
-        }
+    //     if(Product::count()==0){
+    //     // $faker = Faker::create();
+    //     $productsNames = ['something', 'somrthingelse', 'WOW','Amasing'];
+    //     $descriptions = [
+    //     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto accusamus, quam deleniti aut cumque, reprehenderit odio quibusdam earum numquam itaque quisquam labore cupiditate ullam hic doloribus autem nisi non?',
+    //     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto accusamus, quam deleniti aut cumque, reprehenderit odio quibusdam earum numquam itaque quisquam labore cupiditate ullam hic doloribus autem nisi non?',
+    //     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto accusamus, quam deleniti aut cumque, reprehenderit odio quibusdam earum numquam itaque quisquam labore cupiditate ullam hic doloribus autem nisi non?'
+    // ];
+    //     for($i =1; $i <= 18; $i++){
+    //         $category = rand(1,3);
+    //         $product = [
+    //             'id'=>$i,
+    //             'name'=>$productsNames[rand(0,2)],
+    //             'description' => $descriptions[rand(0,2)],
+    //             'price'=> rand(20,1000),
+    //             'size' => rand(1,5),
+    //             'image_id'=> $category,
+    //             'return_policy_id'=> rand(1,4),
+    //             'category_id' => $category
+    //         ];
+    //         Product::create($product);
+    //     }
+    //     }
 
-        if(ProductHasSubcategory::count()==0){
-            for($i = 1; $i<=18;$i++){
-                $pair =[
-                    'product_id'=>  $i,
-                    'subcategory_id'=> rand(1,6)
-                ];
-            ProductHasSubcategory::create($pair);
-            }
-
-
-        }
-
-        if(Address::count()==0){
-            Address::create([
-                'id'=> 1,
-                'user_id' =>1,
-                'address_line_1' => 'bouafia block 716/7',
-                'address_line_2' => ' hbb djelfa',
-                'city' => 'hbb',
-                'state'=> 'djelfa',
-                'country'=>'Algeria ',
-                'postal_code'=>'17023'
-            ]);
-        }
-
-        if (Order::count()==0){
-            Order::create([
-                'id'=>1,
-                'user_id'=>1,
-                'status'=>1,
-                'address_id'=> 1,
-                'total' => 111,
-                'items' => 9
-            ]);
-        }
+        // if(ProductHasSubcategory::count()==0){
+        //     for($i = 1; $i<=18;$i++){
+        //         $pair =[
+        //             'product_id'=>  $i,
+        //             'subcategory_id'=> rand(1,6)
+        //         ];
+        //     ProductHasSubcategory::create($pair);
+        //     }
 
 
-        if (OrderHasProduct::count()==0){
-            OrderHasProduct::create([
-                'order_id'=>1,
-                'product_id'=>1,
-                'quantity'=>3,
-                'price_when_ordered'=>100
-            ]);
-            OrderHasProduct::create([
-                'order_id'=>1,
-                'product_id'=>8,
-                'quantity'=>1,
-                'price_when_ordered'=>100
-            ]);
-            OrderHasProduct::create([
-                'order_id'=>1,
-                'product_id'=>3,
-                'quantity'=>5,
-                'price_when_ordered'=>100
-            ]);
-        }
+        // }
+
+        // if(Address::count()==0){
+        //     Address::create([
+        //         'id'=> 1,
+        //         'user_id' =>1,
+        //         'address_line_1' => 'bouafia block 716/7',
+        //         'address_line_2' => ' hbb djelfa',
+        //         'city' => 'hbb',
+        //         'state'=> 'djelfa',
+        //         'country'=>'Algeria ',
+        //         'postal_code'=>'17023'
+        //     ]);
+        // }
+
+        // if (Order::count()==0){
+        //     Order::create([
+        //         'id'=>1,
+        //         'user_id'=>1,
+        //         'status'=>1,
+        //         'address_id'=> 1,
+        //         'total' => 111,
+        //         'items' => 9
+        //     ]);
+        // }
+
+
+        // if (OrderHasProduct::count()==0){
+        //     OrderHasProduct::create([
+        //         'order_id'=>1,
+        //         'product_id'=>1,
+        //         'quantity'=>3,
+        //         'price_when_ordered'=>100
+        //     ]);
+        //     OrderHasProduct::create([
+        //         'order_id'=>1,
+        //         'product_id'=>8,
+        //         'quantity'=>1,
+        //         'price_when_ordered'=>100
+        //     ]);
+        //     OrderHasProduct::create([
+        //         'order_id'=>1,
+        //         'product_id'=>3,
+        //         'quantity'=>5,
+        //         'price_when_ordered'=>100
+        //     ]);
+        // }
 
 
 
